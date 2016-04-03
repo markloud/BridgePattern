45e9668f3a188f84b2ac72b13ccf0b0ce8b40712
Mark Anthony Chua <MarkAnthony.Chua@infor.com>
2016-04-03 15:46:12 +0800

    initial development

---
+++ BridgePattern/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+class BridgePattern
+{
+    // Shows an abstraction and two implementations proceeding independently
+    interface Bridge
+    {
+        string OperationImp();
+    }
+
+    class Abstraction
+    {
+        Bridge bridge;
+        public Abstraction(Bridge implementation)
+        {
+            bridge = implementation;
+        }
+        public string Operation() // calls the actual operation based on Bridge implementation
+        {
+            return "Abstraction" + " <<< BRIDGE >>>> " + bridge.OperationImp();
+        }
+    }
+
+    class ImplementationA : Bridge
+    {
+        public string OperationImp()
+        {
+            return "ImplementationA";
+        }
+    }
+
+    class ImplementationB : Bridge
+    {
+        public string OperationImp()
+        {
+            return "ImplementationB";
+        }
+    }
+
+    static void Main()
+    {
+        Console.WriteLine("Bridge Pattern\n");
+        Console.WriteLine(new Abstraction(new ImplementationA()).Operation());
+        Console.WriteLine(new Abstraction(new ImplementationB()).Operation());
+
+        Console.ReadKey();
+    }
+}
+/* Output
+Bridge Pattern
+
+Abstraction <<< BRIDGE >>>> ImplementationA
+Abstraction <<< BRIDGE >>>> ImplementationB
+*/
